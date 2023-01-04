d691587bb3d7221b4280f47ac6c488b4579bd368
sarsa <sarsa@LAPTOP-B7NUSF5F>
2023-01-04 15:29:46 +0530

    java execution blocks

--- Assignments/src/assignments/Sample1.java
+++ Assignments/src/assignments/Sample1.java
@@ -1,11 +1,58 @@
 package assignments;
 
+
+
+class Student{
+	
+	static int age;
+	static int id;
+	
+	static{
+		
+		age = 20;
+		id = 40;
+		System.out.println("static block");
+	}
+	
+	static void disp() {
+		
+		System.out.println("static method");
+		System.out.println("a: "+ age + " "+ "b: " +id);
+	}
+	
+	int x;
+	int y;
+	
+	{	
+		x=20;
+		y=30;
+		
+		System.out.println("non static java block");
+	}
+	
+	Student(){
+		
+		System.out.println("constructor");
+	}
+	
+	void disp1() {
+		
+		System.out.println("non static block");
+		System.out.println("x: "+x+" y: "+y);
+	}
+}
+
 public class Sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		System.out.println("Go push!!!");
+		
+		Student.disp();
+		
+		Student st = new Student();
+		st.disp1();
+		
 	}
 
 }
